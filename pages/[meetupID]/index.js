1bd9f567f5af92e0dbe40f48e4a3411aaa79fcaa
Mohammed Falah <falahsss900@gmail.com>
2023-04-29 19:46:48 +0530

    fixed

--- pages/[meetupID]/index.js
+++ pages/[meetupID]/index.js
@@ -1,5 +1,5 @@
 import { MongoClient, ObjectId } from "mongodb";
-import MeetupDetails from "../../components/meetups/MeetUpDetails";
+import MeetupDetails from "@/components/meetups/MeetupDetails";
 import Head from "next/head";
 const MeetupPage = (props) => {
   return (
